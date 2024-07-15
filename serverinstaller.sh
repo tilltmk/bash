04ac7914dcf5503c86476a0fb4dcaebaf62e123e
Till Tomczak <96786986+tilltmk@users.noreply.github.com>
2024-07-15 10:11:47 +0200

    Update serverinstaller.sh

--- serverinstaller.sh
+++ serverinstaller.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get upgrade -y
 # Install Docker
 echo "Installing Docker..."
 apt-get remove docker docker-engine docker.io containerd runc -y
-apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+apt-get install wget git net-tools apt-transport-https ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
